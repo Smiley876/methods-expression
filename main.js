0ba31d3b47da36e5a93d91ac11236a93ccefb3aa
Smiley876 <anzelged16@gmail.com>
2024-12-20 13:03:04 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var name = "Anzel";
+document.write(name.toUpperCase()+"<br>")
+document.write(name.toLowerCase()+"<br>")
+document.write(name.startsWith(`A`)+"<br>")
+document.write(name.startsWith(`a`) + "<br>")
+
+
+document.write(Math.random()+"<br>")
+document.write(Math.floor(54.95868)+"<br>")
+
+var pattern = "anzel schoon"
+var p = "anzel"
+document.write(pattern.replace(p, `Edward`));
